be35819b1b85370444bb75d799e0745befff03f0
Jotakasf <jotakasferreira@gmail.com>
2023-11-14 00:21:31 -0300

    update: py

---
+++ Python/Ex/ex2.py
@@ -0,0 +1,5 @@
+dia = input("Qual dia você nasceu? ")
+mês = input("Qual mês você nasceu? ")
+ano = input("Qual ano você nasceu? ")
+
+print("Você nasceu em", dia, "de", mês, "de", ano )
---
+++ Python/file.py
@@ -0,0 +1,3 @@
+word1 = input("Escreva a primeira palavra: ")
+word2 = input("Escreva a segunda palavra: ")
+print("{}, {}".format(word1, word2))
